0de2b20b5ba6a7ff86908d80cb63defc28935459
StephenB92 <stephennb92@gmail.com>
2022-12-18 18:12:16 +0000

    Deploy to Heroku via CLI

--- app_pages/page_mildew_detector.py
+++ app_pages/page_mildew_detector.py
@@ -25,7 +25,7 @@ def page_mildew_detector_body():
     st.write("---")
 
     images_buffer = st.file_uploader('Upload cherry leaf images. You may select more than one.',
-                                        type='jpg', accept_multiple_files=True)
+                                        type=['jpg', 'png'], accept_multiple_files=True)
    
     if images_buffer is not None:
         df_report = pd.DataFrame([])
